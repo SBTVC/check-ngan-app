b3e2790f24845ac2656950592f43a87d2c2a3ff3
SBTVCIT <chutidetphanit2@gmail.com>
2026-08-24 10:09:12 +0700

    fix: redirect immediately after Clerk sign in

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,11 @@ import { SignIn } from '@clerk/nextjs'
 export default function LoginPage() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-gray-50 p-4">
-      <SignIn routing="hash" />
+      <SignIn
+        routing="hash"
+        forceRedirectUrl="/"
+        signUpForceRedirectUrl="/"
+      />
     </main>
   )
 }
